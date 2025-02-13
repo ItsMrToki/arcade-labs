fbd04f1b48a2b224dcb315fb4a53f61009091eb2
pablo <pablo.rodriguezluna@edu.uah.es>
2025-02-13 16:28:38 +0100

    new draw

--- Pruebas/Dibujo 1.py
+++ Pruebas/Dibujo 1.py
@@ -9,12 +9,10 @@ WIDTH = 1250
 arcade.open_window(1250, 750, "example")
 arcade.set_background_color(arcade.color.PALE_AQUA)
 arcade.start_render()
+
 for i in range(200):
     arcade.draw_circle_filled(randrange(0, WIDTH, 10), randrange(0, HEIGHT, 10), 5, WHITE)
 arcade.draw_circle_filled(625, -1800, 2000, AMAZON, 300, 500)
-
-
-
 
 
 
